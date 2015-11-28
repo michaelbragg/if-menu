8535a1711253b0cac1fe083a5291ce5f719a0838
andrei.igna <andrei.igna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-28 23:02:38 +0000

    code refactoring
    git-svn-id: http://plugins.svn.wordpress.org/if-menu/trunk@1296520 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- if-menu.php
+++ if-menu.php
@@ -33,18 +33,17 @@ class If_Menu {
 	public static function init() {
 		self::$has_custom_walker = 'Walker_Nav_Menu_Edit' !== apply_filters( 'wp_edit_nav_menu_walker', 'Walker_Nav_Menu_Edit' );
 
-		if( is_admin()) {
+		if( is_admin() ) {
 			add_action( 'admin_init', 'If_Menu::admin_init' );
 			add_action( 'wp_update_nav_menu_item', 'If_Menu::wp_update_nav_menu_item', 10, 2 );
 			add_filter( 'wp_edit_nav_menu_walker', create_function( '', 'return "If_Menu_Walker_Nav_Menu_Edit";' ) );
-      add_action( 'wp_nav_menu_item_custom_fields', 'If_Menu::menu_item_fields', 10, 4 );
+      add_action( 'wp_nav_menu_item_custom_fields', 'If_Menu::menu_item_fields' );
       add_action( 'wp_nav_menu_item_custom_title', 'If_Menu::menu_item_title' );
 
       if ( self::$has_custom_walker && 1 != get_option( 'if-menu-hide-notice', 0 ) ) {
         add_action( 'admin_notices', 'If_Menu::admin_notice' );
         add_action( 'wp_ajax_if_menu_hide_notice', 'If_Menu::hide_admin_notice' );
       }
-
 		} else {
 			add_filter( 'wp_get_nav_menu_items', 'If_Menu::wp_get_nav_menu_items' );
 		}
@@ -65,8 +64,7 @@ class If_Menu {
 	}
 
   public static function hide_admin_notice() {
-
-    $re = update_option('if-menu-hide-notice', 1);
+    $re = update_option( 'if-menu-hide-notice', 1 );
 
     echo $re ? 1 : 0;
 
@@ -78,7 +76,9 @@ class If_Menu {
 
 		if( $for_testing ) {
 			$c2 = array();
-			foreach( $conditions as $condition ) $c2[$condition['name']] = $condition;
+			foreach ( $conditions as $condition ) {
+        $c2[$condition['name']] = $condition;
+      }
 			$conditions = $c2;
 		}
 
@@ -89,18 +89,20 @@ class If_Menu {
 		$conditions = If_Menu::get_conditions( $for_testing = true );
 		$hidden_items = array();
 
-		foreach( $items as $key => $item ) {
-			if( in_array( $item->menu_item_parent, $hidden_items ) ) {
+		foreach ( $items as $key => $item ) {
+			if ( in_array( $item->menu_item_parent, $hidden_items ) ) {
 				unset( $items[$key] );
 				$hidden_items[] = $item->ID;
-			} elseif( get_post_meta( $item->ID, 'if_menu_enable', true ) ) {
+			} elseif ( get_post_meta( $item->ID, 'if_menu_enable', true ) ) {
 				$condition_type = get_post_meta( $item->ID, 'if_menu_condition_type', true );
 				$condition = get_post_meta( $item->ID, 'if_menu_condition', true );
 
-				$condition_result = call_user_func( $conditions[$condition]['condition'], $item );
-				if( $condition_type == 'show' ) $condition_result = ! $condition_result;
+				$should_hide_item = call_user_func( $conditions[$condition]['condition'], $item );
+				if ( $condition_type == 'show' ) {
+          $should_hide_item = ! $should_hide_item;
+        }
 
-				if( $condition_result ) {
+				if ( $should_hide_item ) {
 					unset( $items[$key] );
 					$hidden_items[] = $item->ID;
 				}
@@ -113,37 +115,37 @@ class If_Menu {
 	public static function admin_init() {
 		global $pagenow;
 
-    if( $pagenow == 'nav-menus.php' || self::$has_custom_walker ) {
-      wp_enqueue_script( 'if-menu-js', plugins_url( 'if-menu.js' , __FILE__ ), array( 'jquery' ) );
+    if ( $pagenow == 'nav-menus.php' || self::$has_custom_walker ) {
+      wp_enqueue_script( 'if-menu-js', plugins_url( 'if-menu.js', __FILE__ ), array( 'jquery' ) );
     }
 
-		if( $pagenow == 'nav-menus.php' ) {
+		if ( $pagenow == 'nav-menus.php' ) {
       require_once( ABSPATH . 'wp-admin/includes/nav-menu.php' );
       require_once( plugin_dir_path( __FILE__ ) . 'if-menu-nav-menu.php' );
 		}
 	}
 
-  public static function menu_item_fields( $item_id, $item, $depth, $args ) {
+  public static function menu_item_fields( $item_id ) {
     $conditions = If_Menu::get_conditions();
-    $if_menu_enable = get_post_meta( $item->ID, 'if_menu_enable', true );
-    $if_menu_condition_type = get_post_meta( $item->ID, 'if_menu_condition_type', true );
-    $if_menu_condition = get_post_meta( $item->ID, 'if_menu_condition', true );
+    $if_menu_enable = get_post_meta( $item_id, 'if_menu_enable', true );
+    $if_menu_condition_type = get_post_meta( $item_id, 'if_menu_condition_type', true );
+    $if_menu_condition = get_post_meta( $item_id, 'if_menu_condition', true );
     ?>
 
     <p class="if-menu-enable description description-wide">
       <label>
-        <input <?php checked( $if_menu_enable, 1 ) ?> type="checkbox" value="1" class="menu-item-if-menu-enable" name="menu-item-if-menu-enable[<?php echo $item->ID; ?>]" />
+        <input <?php checked( $if_menu_enable, 1 ) ?> type="checkbox" value="1" class="menu-item-if-menu-enable" name="menu-item-if-menu-enable[<?php echo $item_id; ?>]" />
         <?php _e( 'Enable Conditional Logic', 'if-menu' ) ?>
       </label>
     </p>
 
     <p class="if-menu-condition description description-wide" style="display: <?php echo $if_menu_enable ? 'block' : 'none' ?>">
-      <select id="edit-menu-item-if-menu-condition-type-<?php echo $item->ID; ?>" name="menu-item-if-menu-condition-type[<?php echo $item->ID; ?>]">
+      <select id="edit-menu-item-if-menu-condition-type-<?php echo $item_id; ?>" name="menu-item-if-menu-condition-type[<?php echo $item_id; ?>]">
         <option <?php selected( 'show', $if_menu_condition_type ) ?> value="show"><?php _e( 'Show', 'if-menu' ) ?></option>
         <option <?php selected( 'hide', $if_menu_condition_type ) ?> value="hide"><?php _e( 'Hide', 'if-menu' ) ?></option>
       </select>
       <?php _e('if', 'if-menu'); ?>
-      <select id="edit-menu-item-if-menu-condition-<?php echo $item->ID; ?>" name="menu-item-if-menu-condition[<?php echo $item->ID; ?>]">
+      <select id="edit-menu-item-if-menu-condition-<?php echo $item_id; ?>" name="menu-item-if-menu-condition[<?php echo $item_id; ?>]">
         <?php foreach( $conditions as $condition ): ?>
           <option <?php selected( $condition['name'], $if_menu_condition ) ?>><?php echo $condition['name']; ?></option>
         <?php endforeach ?>
@@ -166,7 +168,6 @@ class If_Menu {
       <span class="is-submenu"><?php _e( sprintf('%s if %s', $conditionType, $condition), 'if-menu' ) ?></span>
       <?php
     }
-
   }
 
 	public static function wp_update_nav_menu_item( $menu_id, $menu_item_db_id ) {
