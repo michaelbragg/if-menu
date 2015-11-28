88c914c2c58e78a14f4275a91a128a9f6a4dec4a
andrei.igna <andrei.igna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-28 23:01:42 +0000

    better notice for plugin/theme conflict
    git-svn-id: http://plugins.svn.wordpress.org/if-menu/trunk@1296518 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- if-menu.js
+++ if-menu.js
@@ -4,4 +4,10 @@ jQuery( function( $ ) {
     $( this ).closest( '.if-menu-enable' ).next().toggle( $( this ).prop( 'checked' ) );
   } );
 
+  $( '.wrap' ).on( 'click', '.if-menu-notice button', function() {
+    $.post(ajaxurl, { action: 'if_menu_hide_notice' }, function(response) {
+      alert('Got this from the server: ' + response);
+    });
+  } );
+
 } );
