d9753e7c58cdd08896a6715ecb50a01ed5ec9fe4
andrei.igna <andrei.igna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-28 22:39:11 +0000

    improve Walker_Nav_Menu compatibility
    Use workaround described here
    https://core.trac.wordpress.org/ticket/18584 for better compatibility
    with other plugins/themes
    
    git-svn-id: http://plugins.svn.wordpress.org/if-menu/trunk@1296515 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- if-menu.php
+++ if-menu.php
@@ -9,7 +9,7 @@ Author URI: http://rokm.ro
 License: GPL2
 */
 
-/*  Copyright 2012 More WordPress (email: andrei@rokm.ro)
+/*  Copyright 2012 Andrei Igna (email: andrei@rokm.ro)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
@@ -33,15 +33,19 @@ class If_Menu {
 	public static function init() {
 		self::$has_custom_walker = 'Walker_Nav_Menu_Edit' !== apply_filters( 'wp_edit_nav_menu_walker', 'Walker_Nav_Menu_Edit' );
 
-		if( is_admin() && ! self::$has_custom_walker ) {
+		if( is_admin()) {
 			add_action( 'admin_init', 'If_Menu::admin_init' );
 			add_action( 'wp_update_nav_menu_item', 'If_Menu::wp_update_nav_menu_item', 10, 2 );
 			add_filter( 'wp_edit_nav_menu_walker', create_function( '', 'return "If_Menu_Walker_Nav_Menu_Edit";' ) );
-		} elseif( is_admin() && self::$has_custom_walker ) {
-			add_action( 'admin_notices', 'If_Menu::admin_notices' );
-		} elseif( ! is_admin() && ! self::$has_custom_walker ) {
+      add_action( 'wp_nav_menu_item_custom_fields', 'If_Menu::menu_item_fields', 10, 4 );
+		} else {
 			add_filter( 'wp_get_nav_menu_items', 'If_Menu::wp_get_nav_menu_items' );
 		}
+
+    if ( self::$has_custom_walker ) {
+      add_action( 'admin_notices', 'If_Menu::admin_notices' );
+    }
+
 	}
 
 	public static function admin_notices() {
@@ -90,43 +94,43 @@ class If_Menu {
 
 	public static function admin_init() {
 		global $pagenow;
+
 		if( $pagenow == 'nav-menus.php' ) {
 			wp_enqueue_script( 'if-menu-js', plugins_url( 'if-menu.js' , __FILE__ ), array( 'jquery' ) );
+      require_once( ABSPATH . 'wp-admin/includes/nav-menu.php' );
+      require_once( plugin_dir_path( __FILE__ ) . 'if-menu-nav-menu.php' );
 		}
 	}
 
-	public static function edit_menu_item_settings( $item ) {
-		$conditions = If_Menu::get_conditions();
-		$if_menu_enable = get_post_meta( $item->ID, 'if_menu_enable', true );
-		$if_menu_condition_type = get_post_meta( $item->ID, 'if_menu_condition_type', true );
-		$if_menu_condition = get_post_meta( $item->ID, 'if_menu_condition', true );
-		ob_start();
-		?>
+  public static function menu_item_fields( $item_id, $item, $depth, $args ) {
+    $conditions = If_Menu::get_conditions();
+    $if_menu_enable = get_post_meta( $item->ID, 'if_menu_enable', true );
+    $if_menu_condition_type = get_post_meta( $item->ID, 'if_menu_condition_type', true );
+    $if_menu_condition = get_post_meta( $item->ID, 'if_menu_condition', true );
+    ?>
 
-		<p class="if-menu-enable description description-wide">
-			<label>
-				<input <?php checked( $if_menu_enable, 1 ) ?> type="checkbox" value="1" class="menu-item-if-menu-enable" name="menu-item-if-menu-enable[<?php echo $item->ID; ?>]" />
-				<?php _e( 'Enable Conditional Logic', 'if-menu' ) ?>
-			</label>
-		</p>
+    <p class="if-menu-enable description description-wide">
+      <label>
+        <input <?php checked( $if_menu_enable, 1 ) ?> type="checkbox" value="1" class="menu-item-if-menu-enable" name="menu-item-if-menu-enable[<?php echo $item->ID; ?>]" />
+        <?php _e( 'Enable Conditional Logic', 'if-menu' ) ?>
+      </label>
+    </p>
 
-		<p class="if-menu-condition description description-wide" style="display: <?php echo $if_menu_enable ? 'block' : 'none' ?>">
-			<select id="edit-menu-item-if-menu-condition-type-<?php echo $item->ID; ?>" name="menu-item-if-menu-condition-type[<?php echo $item->ID; ?>]">
-				<option <?php selected( 'show', $if_menu_condition_type ) ?> value="show"><?php _e( 'Show', 'if-menu' ) ?></option>
-				<option <?php selected( 'hide', $if_menu_condition_type ) ?> value="hide"><?php _e( 'Hide', 'if-menu' ) ?></option>
-			</select>
-			<?php _e('if', 'if-menu'); ?>
-			<select id="edit-menu-item-if-menu-condition-<?php echo $item->ID; ?>" name="menu-item-if-menu-condition[<?php echo $item->ID; ?>]">
-				<?php foreach( $conditions as $condition ): ?>
-					<option <?php selected( $condition['name'], $if_menu_condition ) ?>><?php echo $condition['name']; ?></option>
-				<?php endforeach ?>
-			</select>
-		</p>
+    <p class="if-menu-condition description description-wide" style="display: <?php echo $if_menu_enable ? 'block' : 'none' ?>">
+      <select id="edit-menu-item-if-menu-condition-type-<?php echo $item->ID; ?>" name="menu-item-if-menu-condition-type[<?php echo $item->ID; ?>]">
+        <option <?php selected( 'show', $if_menu_condition_type ) ?> value="show"><?php _e( 'Show', 'if-menu' ) ?></option>
+        <option <?php selected( 'hide', $if_menu_condition_type ) ?> value="hide"><?php _e( 'Hide', 'if-menu' ) ?></option>
+      </select>
+      <?php _e('if', 'if-menu'); ?>
+      <select id="edit-menu-item-if-menu-condition-<?php echo $item->ID; ?>" name="menu-item-if-menu-condition[<?php echo $item->ID; ?>]">
+        <?php foreach( $conditions as $condition ): ?>
+          <option <?php selected( $condition['name'], $if_menu_condition ) ?>><?php echo $condition['name']; ?></option>
+        <?php endforeach ?>
+      </select>
+    </p>
 
-		<?php
-		$html = ob_get_clean();
-		return $html;
-	}
+    <?php
+  }
 
 	public static function wp_update_nav_menu_item( $menu_id, $menu_item_db_id ) {
 		$if_menu_enable = isset( $_POST['menu-item-if-menu-enable'][$menu_item_db_id] ) && $_POST['menu-item-if-menu-enable'][$menu_item_db_id] == 1;
@@ -135,28 +139,6 @@ class If_Menu {
 		if( $if_menu_enable ) {
 			update_post_meta( $menu_item_db_id, 'if_menu_condition_type', $_POST['menu-item-if-menu-condition-type'][$menu_item_db_id] );
 			update_post_meta( $menu_item_db_id, 'if_menu_condition', $_POST['menu-item-if-menu-condition'][$menu_item_db_id] );
-		}
-	}
-
-}
-
-
-
-/* ------------------------------------------------
-	Custom Walker for nav items
------------------------------------------------- */
-
-require_once( ABSPATH . 'wp-admin/includes/nav-menu.php' );
-
-class If_Menu_Walker_Nav_Menu_Edit extends Walker_Nav_Menu_Edit {
-
-	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-		$desc_snipp = '<div class="menu-item-actions description-wide submitbox">';
-		parent::start_el( $output, $item, $depth, $args, $id );
-
-		$pos = strrpos( $output, $desc_snipp );
-		if( $pos !== false ) {
-			$output = substr_replace($output, If_Menu::edit_menu_item_settings( $item ) . $desc_snipp, $pos, strlen( $desc_snipp ) );
 		}
 	}
 
